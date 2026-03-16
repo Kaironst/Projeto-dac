54c6306c81efcd17f9de9fa62cc69e8b3ec1d3b4
Bruno Pedron Rupaner <165921892+bruno-pedron@users.noreply.github.com>
2026-03-16 12:05:39 +0000

    implementação do frontend de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { TelaPrincipal } from './tela_principal/tela_principal';
 import { Autocadastro } from './autocadastro/autocadastro';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     { path:'tela-principal', component:TelaPrincipal},
     { path: '', redirectTo:'tela-principal', pathMatch:'full'},
-    { path: 'autocadastro', component: Autocadastro }
+    { path: 'autocadastro', component: Autocadastro },
+    { path: 'login', component: Login }
 ];
