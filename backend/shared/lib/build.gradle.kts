b5bf8a6b500cada31b0e8684b927ecdcf30f2ddb
Kaironst <pedronovak4@gmail.com>
2026-04-10 21:43:34 -0300

    alterado configuração de push do shared

--- backend/shared/lib/build.gradle.kts
+++ backend/shared/lib/build.gradle.kts
@@ -38,26 +38,19 @@ java {
 }
 
 publishing {
-  publications{
-    create<MavenPublication>("gpr"){
-      from(components["java"])
-      
-      groupId="br.ufpr.dac"
-      artifactId="shared"
-      version="1.0.0"
-
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
     }
-  }
-  repositories {
-    maven {
-      name = "GitHubPackages"
-      url = uri("https://maven.pkg.github.com/kaironst/projeto-dac")
-
-      credentials {
-        username = System.getenv("GITHUB_USER")
-        password = System.getenv("GITHUB_TOKEN")
-      }
-
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
     }
-  }
 }
