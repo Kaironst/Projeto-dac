72eaa9cfde05a4894f6479eefb6fcbe35e8b8e59
Kaironst <pedronovak4@gmail.com>
2026-04-20 22:18:32 -0300

    formato de dto retornado ao dto exclusivo de ItemHistorico (achei mais fácil manter consistência assim)

--- backend/shared/lib/src/main/java/br/ufpr/dac/shared/dto/ItemHistoricoDto.java
+++ backend/shared/lib/src/main/java/br/ufpr/dac/shared/dto/ItemHistoricoDto.java
@@ -1,9 +1,8 @@
 package br.ufpr.dac.shared.dto;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import br.ufpr.dac.shared.dto.GerentesDto.Gerente;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,17 +10,16 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-public class ContasDto {
-
+public class ItemHistoricoDto {
   @Data
   @EqualsAndHashCode(callSuper = false)
   @SuperBuilder
-  @AllArgsConstructor
   @NoArgsConstructor
+  @AllArgsConstructor
   public static class Message extends MessageWrapper {
-    private List<Conta> data;
+    private List<ItemHistorico> data;
 
-    public Message(String operation, List<Conta> data) {
+    public Message(String operation, List<ItemHistorico> data) {
       super(operation);
       this.data = data;
     }
@@ -30,16 +28,15 @@ public class ContasDto {
 
   @Data
   @Builder
-  @AllArgsConstructor
   @NoArgsConstructor
-  public static class Conta {
+  @AllArgsConstructor
+  public static class ItemHistorico {
     private Long id;
-    private UsersDto.Cliente cliente;
-    private Gerente gerente;
-    private Double saldo;
-    private Double limite;
-    private LocalDate dataCriacao;
-
+    private ContasDto.Conta contaOrigem;
+    private ContasDto.Conta contaDestino;
+    private LocalDateTime dataHora;
+    private Integer tipo;
+    private Double valorMovimentacao;
   }
 
 }
