9290a585686d5aae7c3efcf20f29d1b0325877cc
Kaironst <pedronovak4@gmail.com>
2026-06-09 20:24:59 -0300

    auth controller agora faz a query de maneira correta para o cqrs   o payload retornado é de acordo com a especificação do projeto

--- backend/apiGatewayServiceNode/src/controller/AuthController.ts
+++ backend/apiGatewayServiceNode/src/controller/AuthController.ts
@@ -1,11 +1,17 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, response } from "express";
 import { LoginRequest } from "../dto/LoginRequest";
-import { authProducer } from "../messaging/GenericProducerRPC";
+import { authProducer, gerentesProducerCqrs, usersProducerCqrs } from "../messaging/GenericProducerRPC";
+import { TokenDto } from "../dto/TokenDto";
+import { validateJwt } from "../auth/JwtUtils";
+import { validateHeaderValue } from "http";
+import { jwtDecode } from "jwt-decode";
+import { UsersDtoCliente } from "../dto/UsersDto";
+import { GerentesDtoGerente } from "../dto/GerentesDto";
 
 const router = Router();
 
 //POST /
-router.post("/", async (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   try {
     const loginRequest = req.body as LoginRequest;
     const responseToken = await authProducer.requestService({ operation: "LOGIN", data: [loginRequest], dataType: "login" });
@@ -17,17 +23,33 @@ router.post("/", async (req: Request, res: Response) => {
       return;
     }
 
+    const decoded = jwtDecode(responseToken.data?.at(0)?.token!);
+    const username = decoded.sub!;
+
+    const roles: string[] = (decoded as any).roles || [];
+    if (roles.length === 0)
+      throw new Error("token inválido (sem roles)")
+
+    let user: UsersDtoCliente | GerentesDtoGerente | null = null;
+    let tipo: string | null = null;
+
+    if (roles.includes("ROLE_CLIENTE")) {
+      user = (await usersProducerCqrs.requestService({ operation: "READ_BY_EMAIL", data: [{ email: username } as UsersDtoCliente], dataType: "cliente" })).data![0];
+      tipo = "CLIENTE"
+    }
+    else if (roles.includes("ROLE_GERENTE") || roles.includes("ROLE_ADMINISTRADOR")) {
+      user = (await gerentesProducerCqrs.requestService({ operation: "READ_BY_EMAIL", data: [{ email: username } as GerentesDtoGerente], dataType: "gerente" })).data![0];
+      tipo = roles.includes("ROLE_ADMINISTRADOR") ? "ADMINISTRADOR" : "GERENTE"
+    }
+
     const loginResponse = {
       access_token: responseToken.data?.at(0)?.token,
       token_type: "bearer",
 
-      tipo: "CLIENTE",
+      tipo: tipo,
 
-      usuario: {
-        nome: "Catharyna",
-        cpf: "12912861012",
-        email: "cli1@bantads.com.br"
-      }
+      usuario: user
+
     };
 
     res.status(200).json(loginResponse);
@@ -35,5 +57,20 @@ router.post("/", async (req: Request, res: Response) => {
     res.sendStatus(500);
   }
 });
+
+router.post("/validate", async (req: Request, res: Response) => {
+  try {
+
+    const body = req.body as { token: string };
+    const validated = validateJwt(body.token);
+
+    validated ? res.status(200).json(true) :
+      res.status(401).json(false);
+
+  }
+  catch (error) {
+    res.sendStatus(500);
+  }
+})
 
 export default router;
